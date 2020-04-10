a19a7e9dafede131c4d2857f527d2f214e32b741
linux_china <libing.chen@gmail.com>
2020-04-09 17:07:53 -0700

    Portal controller

---
+++ rsocket-requester/src/main/java/com/alibaba/spring/boot/rsocket/demo/PortalController.java
@@ -0,0 +1,58 @@
+package com.alibaba.spring.boot.rsocket.demo;
+
+import com.alibaba.user.User;
+import com.alibaba.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+/**
+ * portal demo for test
+ *
+ * @author leijuan
+ */
+@RestController
+public class PortalController {
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/error")
+    public Mono<String> error() {
+        return userService.error("Hi");
+    }
+
+    @GetMapping("/job1")
+    public Mono<Void> job1() {
+        return userService.job1();
+    }
+
+    @RequestMapping("/appName")
+    public Mono<String> appName() {
+        return userService.getAppName();
+    }
+
+    @RequestMapping("/flux")
+    public Flux<User> flux() {
+        return userService.findAllPeople("vip");
+    }
+
+    @RequestMapping("/channel1")
+    public Flux<User> channel1() {
+        Flux<Integer> userIdFlux = Flux.range(1, 20);
+        return userService.recent(userIdFlux);
+    }
+
+    @RequestMapping("/channel2")
+    public Flux<User> channel2() {
+        Flux<Integer> userIdFlux = Flux.range(1, 20);
+        return userService.recentWithType("VIP", userIdFlux);
+    }
+
+    @RequestMapping("/")
+    public String index() {
+        return "This is RSocket Requester App!";
+    }
+}
