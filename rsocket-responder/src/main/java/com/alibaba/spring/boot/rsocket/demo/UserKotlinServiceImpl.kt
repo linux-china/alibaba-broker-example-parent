da2b16e4a5979bb14450ec9e755d4abbb6bad0fa
linux_china <libing.chen@gmail.com>
2020-04-11 14:44:22 -0700

    add channel

--- rsocket-responder/src/main/java/com/alibaba/spring/boot/rsocket/demo/UserKotlinServiceImpl.kt
+++ rsocket-responder/src/main/java/com/alibaba/spring/boot/rsocket/demo/UserKotlinServiceImpl.kt
@@ -4,6 +4,7 @@ import com.alibaba.rsocket.RSocketService
 import com.alibaba.user.UserKotlinService
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.map
 import org.springframework.stereotype.Service
 
 /**
@@ -14,19 +15,21 @@ import org.springframework.stereotype.Service
 @Service
 @RSocketService(serviceInterface = UserKotlinService::class)
 class UserKotlinServiceImpl : UserKotlinService {
-    override suspend fun getOwner(): String {
-        return "leijuan"
+    override suspend fun job1() {
+        println("job1")
     }
 
     override suspend fun getNickById(id: Int): String {
         return "nick: $id"
     }
 
-    override fun getAllNames(): Flow<String> {
-        return arrayOf("first", "second").asFlow()
-    }
-
     override fun findNamesByType(type: Int): Flow<String> {
         return arrayOf("first", "second", "type: $type").asFlow();
+    }
+
+    override fun findNamesByIdFlow(idFlow: Flow<Int>): Flow<String> {
+        return idFlow.map {
+            "nick: $it"
+        }
     }
 }
