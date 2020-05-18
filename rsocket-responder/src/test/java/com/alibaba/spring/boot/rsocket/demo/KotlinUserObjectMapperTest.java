1caf43c83e4462d18b029918ad9cbaae347767f7
linux_china <libing.chen@gmail.com>
2020-05-17 18:17:03 -0700

    add more test methods

--- rsocket-responder/src/test/java/com/alibaba/spring/boot/rsocket/demo/KotlinUserObjectMapperTest.java
+++ rsocket-responder/src/test/java/com/alibaba/spring/boot/rsocket/demo/KotlinUserObjectMapperTest.java
@@ -1,9 +1,17 @@
 package com.alibaba.spring.boot.rsocket.demo;
 
+import com.alibaba.account.Account;
 import com.alibaba.user.KotlinUser;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.cbor.CBORFactory;
+import com.google.protobuf.GeneratedMessageV3;
+import kotlinx.serialization.KSerializer;
+import kotlinx.serialization.SerializationStrategy;
+import kotlinx.serialization.json.Json;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
 
 /**
  * Kotlin User ObjectMapper test
@@ -12,6 +20,8 @@ import org.junit.jupiter.api.Test;
  */
 public class KotlinUserObjectMapperTest {
     private static ObjectMapper objectMapper = new ObjectMapper();
+    private static ObjectMapper cborMapper = new ObjectMapper(new CBORFactory());
+
 
     @BeforeAll
     public static void setUp() {
@@ -25,5 +35,35 @@ public class KotlinUserObjectMapperTest {
         System.out.println(jsonText);
         KotlinUser kotlinUser = objectMapper.readValue(jsonText, KotlinUser.class);
         System.out.println(kotlinUser.getNick());
+    }
+
+    @Test
+    public void testKtClass() throws Exception {
+        KSerializer<?> serializer = getSerializer(KotlinUser.class);
+        KotlinUser user = new KotlinUser(1, "linux_china", "email", "186");
+        String jsonText = Json.Default.stringify((SerializationStrategy<? super KotlinUser>) serializer, user);
+        System.out.println(jsonText);
+    }
+
+    public KSerializer<?> getSerializer(Class<?> clazz) throws Exception {
+        Class<?> serializerClazz = Class.forName(clazz.getCanonicalName() + "$$serializer");
+        Field instanceField = serializerClazz.getDeclaredField("INSTANCE");
+        return (KSerializer<?>) instanceField.get(null);
+    }
+
+    @Test
+    public void testProtobufClass() {
+        Class<Account> clazz = Account.class;
+        boolean assignableFrom = GeneratedMessageV3.class.isAssignableFrom(clazz);
+        System.out.println(assignableFrom);
+    }
+
+    @Test
+    public void testCbor() throws Exception {
+        KotlinUser user = new KotlinUser(1, "linux_china", "xxx@yyy.com", "186");
+        byte[] bytes = cborMapper.writeValueAsBytes(user);
+        System.out.println(bytes.length);
+        KotlinUser user2 = cborMapper.readValue(bytes, KotlinUser.class);
+        System.out.println(user2.getNick());
     }
 }
